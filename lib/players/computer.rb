3cddeaded82768d5cc4c59dc609d5ba53cbcbd2e
Sara Nolen <nolensara1@gmail.com>
2021-03-07 01:16:52 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -10,24 +10,20 @@ module Players
      elsif !board.taken?(1)
        move = "1"
 
-     elsif (board.position(1) == board.position(3) || board.position(5) == board.position(8))
+     elsif (board.position(1) == board.position(3) || board.position(5) == board.position(8)) && !board.taken?(2)
        move = "2" 
-     elsif (board.position(5) == board.position(2) || board.position(7) == board.position(9))
+     elsif (board.position(5) == board.position(2) || board.position(7) == board.position(9)) && !board.taken?(8)
        move = "8"
-     elsif (board.position(5) == board.position(6) || board.position(1) == board.position(7))
+     elsif (board.position(5) == board.position(6) || board.position(1) == board.position(7)) && !board.taken?(4)
        move = "4"
-      elsif (board.position(4) == board.position(5) || board.position(3) == board.position(9))
+     elsif (board.position(4) == board.position(5) || board.position(3) == board.position(9)) && !board.taken?(6)
        move = "6"
-      elsif (board.position(3) == board.position(5) || board.position(8) == board.position(9))
+     elsif (board.position(3) == board.position(5) || board.position(8) == board.position(9)) && !board.taken?(7)
        move = "7"
-     elsif !board.taken?(3)
-       move = "3"
-     elsif !board.taken?(7)
-       move = "7"
-     elsif !board.taken?(9)
-       move = "9"
-       
+     elsif (board.position(1) == board.position(2) || board.position(6) == board.position(9)) && !board.taken?(3)
+      move = "3"
     else
+      
       end
     end
 
